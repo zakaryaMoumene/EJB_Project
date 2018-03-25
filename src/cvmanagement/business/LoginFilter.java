681d0d6fcd88e010141fe3667e23e637e820dd66
Zakarya Moumene <zakarya.moumene@gmail.com>
2018-03-26 01:04:14 +0200

    fixes

--- src/cvmanagement/business/LoginFilter.java
+++ src/cvmanagement/business/LoginFilter.java
@@ -11,44 +11,27 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Filter checks if LoginBean has loginIn property set to true. If it is not set
- * then request is being redirected to the login.xhml page.
- * 
- * @author itcuties
- *
- */
 public class LoginFilter implements Filter {
 
-    /**
-     * Checks if user is logged in. If not it redirects to the login.xhtml page.
-     */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        // Get the loginBean from session attribute
-        // For the first application request there is no loginBean in the
-        // session so user needs to log in
-        // For other requests loginBean is present but we need to check if user
-        // has logged in successfully
+        String contextPath = ((HttpServletRequest) request).getContextPath();
         if (null == ((HttpServletRequest) request).getSession().getAttribute("loggedUser")) {
-            String contextPath = ((HttpServletRequest) request).getContextPath();
-            System.out.println(contextPath + "/login");
+            
             ((HttpServletResponse) response).sendRedirect(contextPath + "/login.xhtml");
             return;
         }
         try {
             chain.doFilter(request, response);
         } catch (Exception e) {
-            ((HttpServletResponse) response).sendRedirect("errorPage.xhtml");
+            ((HttpServletResponse) response).sendRedirect(contextPath+"/errorPage.xhtml");
         }
     }
 
     public void init(FilterConfig config) throws ServletException {
-        // Nothing to do here!
     }
 
     public void destroy() {
-        // Nothing to do here!
     }
 
 }
